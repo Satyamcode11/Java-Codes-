0676692da6dffd435340f5b863601eafe882f7c0
Satyamcode11 <moharanasatyam11@gmail.com>
2026-08-24 00:23:28 +0530

    Practice Exception Handling

---
+++ Exception_Handling/src/Exception_prac.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exception_prac {
+    public static void main(String[] args) {
+        try {
+            int var1 = 6 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("After exception Handling Message ");
+        System.out.println();
+        String string = null;
+
+        try {
+            int length = string.length();
+            System.out.println("length: " + length);
+        } catch (NullPointerException var23) {
+            System.out.println("Here it catch an exception");
+        }
+
+        System.out.println("The program continue running safely: ");
+
+        try {
+            int[] arr = new int[]{10, 20, 30, 40};
+            System.out.println(arr[50]);
+            int var3 = 40 / 0;
+        } catch (ArrayIndexOutOfBoundsException var21) {
+            System.out.println("Here Arrays out of bound exception catch");
+        } catch (ArithmeticException var22) {
+            System.out.println("Here catch an arithmetic exception");
+        }
+
+        System.out.println();
+
+        try {
+            String str = "Satyam";
+            int parsedNumber = Integer.parseInt(str);
+            int[] arr = new int[]{1, 2, 3, 4};
+            System.out.println(arr[5]);
+            int var5 = 50 / 0;
+        } catch (ArrayIndexOutOfBoundsException | ArithmeticException | NumberFormatException e) {
+            System.out.println(((RuntimeException) e).getMessage());
+        }
+
+        System.out.println();
+
+        try {
+            int e = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("num" + e.getMessage());
+        } finally {
+            System.out.println("I always execute no matter exception occurs or not");
+        }
+
+        System.out.println();
+        File file = new File("git.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException var26) {
+            System.out.println("Error: Target file could not be found on desk");
+        }
+
+    }
+
+}
+
+
+
