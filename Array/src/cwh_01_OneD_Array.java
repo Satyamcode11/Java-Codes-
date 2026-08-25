d9ff4d0421ab5a66947dad0c6a9123278f3611cd
Satyamcode11 <moharanasatyam11@gmail.com>
2026-08-25 12:17:40 +0530

    Practice 1D Array

---
+++ Array/src/cwh_01_OneD_Array.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class cwh_01_OneD_Array {
+    public static void main(String[] args) {
+     //Array- It is linear data structure that store a collection of elements of
+        // same data type in a contiguous location under a single variable name.
+        // Traditional Array are fixed in capacity once allocated meaning their
+        // length is fixed but the stored value can be updated.
+
+        //example: 1
+        int [] arr = new int [5];
+        arr[0]= 10;
+        arr[1]= 10;
+        arr[2]= 10;
+        arr[3]= 10;
+        arr[4]= 10;
+        System.out.println(arr.length);
+        for(int numbers:arr) {
+            System.out.print(numbers + ", ");
+        }
+
+        System.out.println();
+
+
+            //example: 2 (Traversing/Accessing an array)
+            String [] array = {"Java", "Kotlin", "C++", "Python"};
+            System.out.println(array.length);
+             for(int i=0;i<array.length;i++){
+                 System.out.println(array[i]);
+             }
+
+        System.out.println();
+
+             //example: 3 (Reverse an array using while loop)
+        int x =array.length-1;
+        while(x>=0){
+            System.out.print(array[x]+ ", ");
+            x--;
+        }
+
+        System.out.println();
+
+        //example: 4
+        int [] numbers = {10,20,30,47,59,60};
+
+        int i=0;
+        while(i<numbers.length){
+            if (numbers[i]%2==0){
+                System.out.println(numbers[i] + ", ");
+            }
+            i++;
+        }
+
+        System.out.println();
+
+        //example: 5 (Array to arraylist)
+      ArrayList<Integer> al = new ArrayList<>();
+      for (int num: numbers){
+          al.add(num);
+      }
+        System.out.println(al);
+
+    }
+}
